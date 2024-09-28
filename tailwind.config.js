0f11fb286e1e441acb32d2f24aa606990c4498c3
aryans0714 <aryan.sharma0714@gmail.com>
2024-09-28 12:21:00 -0400

    Font setup, tailwind config pt2

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,16 @@ module.exports = {
     extend: {
       colors: {
         primary: '#001c1c',
-        fontColor: '#e0ffff',
+        font: '#e0ffff',
         secondary: '#7f7fff',
         accent: '#ff5c00',
         disabled: '#a9a9a9',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      fontWeight: {
+        light: 200,
       },
     },
   },
