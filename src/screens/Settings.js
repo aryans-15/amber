c1bff88cc416813969a8fa4931f4fa6681e8f773
victor <victorliu5000@gmail.com>
2024-09-29 06:40:52 -0400

    settings updated

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 function Settings() {
   const [language, setLanguage] = useState("english");
@@ -12,6 +13,8 @@ function Settings() {
   const [userEmail, setUserEmail] = useState(null);
 
   const auth = getAuth();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
@@ -51,12 +54,18 @@ function Settings() {
 
     try {
       const updatedData = { language, speed, voice };
-      await setDoc(userDocRef, updatedData, { merge: true });
-      console.log("Settings updated successfully!");
+      await setDoc(userDocRef, updatedData, { merge: true }).then(() => {
+        alert("Settings updated");
+      });
     } catch (error) {
       console.error("Error updating document: ", error);
       console.log("Failed to update settings");
     }
+    navigate("/home");
+  };
+
+  const handleCancel = () => {
+    navigate("/home");
   };
 
   return (
@@ -64,26 +73,36 @@ function Settings() {
       <Header />
       <div className="h-full w-full flex flex-grow items-center justify-center">
         <div className="flex flex-col p-8 text-disabled w-[40%] h-[50%] border-4 border-font shadow-lg shadow-font rounded-lg">
+          <h2 class="mb-8 text-4xl tracking-tight font-extrabold text-white">
+            Settings
+          </h2>
+          <div class="pt-8 text-left border-t border-gray-700"></div>
           <div className="flex flex-col space-y-4">
             <div className="flex space-x-4 items-center justify-between">
-              <p className="text-disabled text-2xl">Language: </p>
+              <p className="text-white text-2xl">Language: </p>
               <select
-                className="bg-disabled text-font border-4 border-font text-xl rounded-lg block w-[80%] py-2 px-3"
+                className="bg-secondary text-white border-4 border-font text-xl rounded-lg block w-[80%] py-2 px-3 hover:cursor-pointer"
                 value={language}
                 onChange={(e) => setLanguage(e.target.value)}
               >
-                <option value="english">English</option>
-                <option value="spanish">Spanish</option>
-                <option value="chinese">Chinese</option>
-                <option value="french">French</option>
-                <option value="korean">Korean</option>
-                <option value="japanese">Japanese</option>
+                <option value="en-US">English</option>
+                <option value="es-AR">Spanish</option>
+                <option value="zh-CN">Chinese</option>
+                <option value="fr-FR">French</option>
+                <option value="ko-KR">Korean</option>
+                <option value="ja-JP">Japanese</option>
+                <option value="pt-BR">Portuguese</option>
+                <option value="hi-IN">Hindi</option>
+                <option value="ta-IN">Tamil</option>
+                <option value="te-IN">Telugu</option>
+                <option value="ar-IQ">Arabic</option>
+                <option value="de-DE">German</option>
               </select>
             </div>
             <div className="flex space-x-4 items-center justify-between">
-              <p className="text-disabled text-2xl">Speed: </p>
+              <p className="text-white text-2xl">Speed: </p>
               <select
-                className="bg-disabled text-font border-4 border-font text-xl rounded-lg block w-[80%] py-2 px-3"
+                className="bg-secondary text-white border-4 border-font text-xl rounded-lg block w-[80%] py-2 px-3 hover:cursor-pointer"
                 value={speed}
                 onChange={(e) => setSpeed(e.target.value)}
               >
@@ -93,9 +112,9 @@ function Settings() {
               </select>
             </div>
             <div className="flex space-x-4 items-center justify-between">
-              <p className="text-disabled text-2xl">Voice: </p>
+              <p className="text-white text-2xl">Voice: </p>
               <select
-                className="bg-disabled text-font border-4 border-font text-xl rounded-lg block w-[80%] py-2 px-3"
+                className="bg-secondary text-white border-4 border-font text-xl rounded-lg block w-[80%] py-2 px-3 hover:cursor-pointer"
                 value={voice}
                 onChange={(e) => setVoice(e.target.value)}
               >
@@ -108,11 +127,14 @@ function Settings() {
           </div>
 
           <div className="flex justify-end space-x-4 mt-auto">
-            <button className="bg-accent hover:bg-hovera px-4 py-2 rounded-lg text-xl text-font duration-300">
+            <button
+              className="bg-secondary hover:bg-hoverc px-4 py-2 rounded-lg text-xl text-font duration-300"
+              onClick={handleCancel}
+            >
               Cancel
             </button>
             <button
-              className="bg-accent hover:bg-hovera px-4 py-2 rounded-lg text-xl text-font duration-300"
+              className="bg-secondary hover:bg-hoverc px-4 py-2 rounded-lg text-xl text-font duration-300"
               onClick={handleSave}
             >
               Save
